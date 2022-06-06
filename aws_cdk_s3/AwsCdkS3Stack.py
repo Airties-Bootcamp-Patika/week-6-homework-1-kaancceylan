e60cc9f696a945daa7220942901a57d322e4bda2
Kaan Ceylan <70446695+kaancceylan@users.noreply.github.com>
2022-06-06 17:22:05 +0300

    Add files via upload

---
+++ aws_cdk_s3/AwsCdkS3Stack.py
@@ -0,0 +1,32 @@
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
+            
+class AwsCdkS3Stack(cdk.Stack):
+
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
+
+
+""" from aws-cdk-lib import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class AwsCdkS3Stack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "AwsCdkS3Queue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+        #  
+        # """
